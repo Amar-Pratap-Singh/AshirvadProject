f9aa708302a0a19d681cd3d217d1ab068992eb0b
Amar Pratap Singh <75470134+Amar-Pratap-Singh@users.noreply.github.com>
2022-10-03 16:24:20 +0530

    Add files via upload

--- Backend/app.js
+++ Backend/app.js
@@ -86,16 +86,16 @@ app.post('/SignIn', async (req, res) =>{
     try{
         const id = req.body.username;
         const password = req.body.Password;
-        
+        const role = req.body.Role;
         // get collection
         // const usersDetails = await db.collection('RegisteredUsers').get();
 
         // get document
-        const user = await db.collection('RegisteredUsers').doc(id).get();
+        const user = await db.collection('Registered'+role).doc(id).get();
         let response = 0;
 
         if (!user.exists) {
-            console.log('Incorrect UserName!');
+            console.log('User Not Registered!');
         } 
         else {
             userDataJson = user.data();
@@ -109,18 +109,88 @@ app.post('/SignIn', async (req, res) =>{
         }
 
         if(response) {
-            res.json({ status: "ok" }) 
+            // res.json({userData: user.data()});
+            res.json({ status: "ok", userData: user.data()}) 
         }
         else {
             res.json({ status: "error" })
         }
-
     }
     catch(error){
         res.send({status: error});
         return;
     }
 })
+
+
+
+
+
+
+app.post('/RegisterNewRole', async (req, res) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    try{
+        const id = req.body.username;
+        const password = req.body.Password;
+        const role = req.body.Role;
+
+        const userCredsJson = {
+            username: req.body.username,
+            password: req.body.Password
+        };
+
+        const user = await db.collection('RegisteredUsers').doc(id).get();
+        let verify = 1;
+        let userRoles = user.data().role;
+
+        if (!user.exists) {
+            console.log('User Not Registered!');
+        } 
+        
+        else {
+            userDataJson = user.data();
+            if (userDataJson.password === password){
+                // console.log("success!");
+
+                for (let roleIndex = 0; roleIndex < 3 ; roleIndex++){
+                    if (role[roleIndex] !== ""){
+                        userRoles[roleIndex] = role[roleIndex];
+                        response = await db.collection("Registered"+role[roleIndex]).doc(id).set(userCredsJson);
+                        if (!response)
+                            verify = 0;
+                    }
+                }
+            }
+            else{
+                console.log("Incorrect password!");
+            }
+        }
+
+        const liam = await db.collection('RegisteredUsers').doc(id).set({
+            role: userRoles
+        }, { merge: true });
+
+        if(verify) {
+            // res.json({userData: user.data()});
+            res.json({ status: "ok", userData: user.data()}) 
+        }
+        else {
+            res.json({ status: "error" })
+        }
+    }
+    catch(error){
+        res.send({status: error});
+        return;
+    }
+})
+
+
+
+
+
 
 
 app.post('/SignUp', async (req, res) =>{
@@ -136,23 +206,48 @@ app.post('/SignUp', async (req, res) =>{
             username: req.body.username,
             email: req.body.Email,
             contact: req.body.Contact,
+            password: req.body.Password,
+            role: req.body.Role
+        };
+
+        const userCredsJson = {
+            username: req.body.username,
             password: req.body.Password
         };
-        
-        const response = await db.collection("RegisteredUsers").doc(id).set(userJson);
-        if(response) {
-            res.json({ status: "ok" }) 
-        }
-        else {
-            res.json({ status: "error" })
-        }
 
+
+        const user = await db.collection('RegisteredUsers').doc(id).get();
+
+        if (user.exists) {
+            console.log('Username already taken!');
+        } 
+        else {
+            let response = await db.collection("RegisteredUsers").doc(id).set(userJson);
+            let verify = 1;
+            
+            for (let roleIndex = 0; roleIndex < 3 ; roleIndex++){
+                if (userJson.role[roleIndex] !== ""){
+                    response = await db.collection("Registered"+userJson.role[roleIndex]).doc(id).set(userCredsJson);
+                    if (!response)
+                        verify = 0;
+                }
+            }
+    
+            if(verify) {
+                res.json({ status: "ok" }) 
+            }
+            else {
+                res.json({ status: "error" })
+            }
+        }
     }
     catch(error){
         res.send({status: error});
         return;
     }
 })
+
+
 
 
 const PORT = process.env.PORT || 8080;
