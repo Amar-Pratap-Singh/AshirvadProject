3ce987ce13e6cbb38ca5585052744b84e0797bce
Amar Pratap Singh <75470134+Amar-Pratap-Singh@users.noreply.github.com>
2022-10-07 15:01:27 +0530

    Add files via upload

---
+++ Frontend/src/ComplaintCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function ComplaintCard(props){
+
+    function eventHandler(){
+        alert("Accepted Complaint with Complaint ID: " + props.complaintID);
+        
+        const updatedComplaints = props.complaints.filter((_, i) => i !== props.index);
+        const updatedComplaintIDs = props.complaintIDs.filter((_, i) => i !== props.index);
+
+        
+        
+        const complaintID = props.complaintID;
+        const plumberUsername = props.plumberUsername;
+        
+        const result = fetch("http://localhost:8080/AcceptComplaint", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                complaintID,
+                plumberUsername,
+                updatedComplaintIDs
+            })
+        }).then((res) => res.json());
+        
+        console.log(result.status);
+        props.setComplaints(updatedComplaints);
+        props.setComplaintIDs(updatedComplaintIDs);
+        console.log(updatedComplaintIDs);
+        console.lof(props.complaintIDs);
+    }
+
+    return(
+        <div class="card">
+            <h5 class="card-header">Complaint ID: {props.complaintID}</h5>
+            <div class="card-body">
+                <h5 class="card-title">Raised By: {props.customerUsername}</h5>
+                <p class="card-text">{props.complaint}</p>
+                <button onClick={eventHandler} class="btn btn-primary">Accept</button>
+            </div>
+        </div>
+    );
+}
+
+export default ComplaintCard;
---
+++ Frontend/src/Plumber/myComplaints.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ComplaintCard from '../ComplaintCard';
+import Navbar from '../Navbar';
+import { useState } from 'react';
+
+function MyComplaints(props){
+
+    const [complaints, setComplaints] =  useState(props.assignedComplaints);
+    const [complaintIDs, setComplaintIDs] = useState(props.assignedComplaintIDs);
+
+    return (
+        <div>
+            <Navbar props={props}/>
+
+            <div class="center_div">
+                <h1>My Complaints</h1>
+                
+                {complaints.map((complaint, index) => (
+                    <div>
+                        <ComplaintCard index={index} plumberUsername={props.userDetails.userName} complaints={complaints} complaintIDs={complaintIDs} setComplaintIDs={setComplaintIDs} setComplaints={setComplaints} customerUsername={props.customers[index]} complaintID={complaintIDs[index]} complaint={complaint} />
+                        <br></br>
+                    </div>
+                ))}
+             
+            </div>
+        </div>
+    );
+}
+
+export default MyComplaints;
